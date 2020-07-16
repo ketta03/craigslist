48cce560ef165e163dd100df5f2d4a530e96c85c
ketta03 <61695642+ketta03@users.noreply.github.com>
2020-07-16 12:47:20 +0300

    new

--- my_app/views.py
+++ my_app/views.py
@@ -1,15 +1,23 @@
 import requests
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+from requests.compat import quote_plus
+
+BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
+
 
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
 
+
 def new_search(request):
     search = request.POST.get('search')
-    print(search)
-    stuff_for_frontend ={
+    response = requests.get('https://losangeles.craigslist.org/search/bbb?query=python%20tutor&sort=rel')
+    data = response.txt
+    print(data)
+
+    stuff_for_frontend = {
         'search': search,
     }
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
